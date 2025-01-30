6ec06e0280a8c86d69abdf5cf2a5946d4cc2a834
Dias-Gabriel-Dev <gabriel.henrique.dias@outlook.com>
2025-01-30 11:51:38 -0300

    Novas tarefas pytthon

---
+++ controle-de-estoque/controle-de-estoque.py
@@ -0,0 +1,8 @@
+estoque = 5
+
+while estoque > 0:
+    print(f"Venda realizada! Estoque restante: {estoque}")
+    estoque -= 1
+    print("Estoque esgotado")
+
+    
---
+++ organizador/organizador.py
@@ -0,0 +1,7 @@
+projetos = ["website", "jogo", "análise de dados", None, "aplicativo móvel"]
+
+for projetos in projetos:
+    if projetos is None:
+        print("Projeto ausente")
+    else:
+        print(projetos)
